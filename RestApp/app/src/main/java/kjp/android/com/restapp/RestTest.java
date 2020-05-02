5625ed457afa294e83fd5ead8bb993fce91aa4e2
DESKTOP-6HO4AK1\ghaga <ironypj@gmail.com>
2020-05-02 18:53:35 +0900

    AndroidProject Using RestAPI from SpringMVC Server -- RestApp

---
+++ RestApp/app/src/main/java/kjp/android/com/restapp/RestTest.java
@@ -0,0 +1,28 @@
+package kjp.android.com.restapp;
+
+public class RestTest {
+
+    private String name;
+    private int age;
+    private String currentTime;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getCurrentTime() {
+        return currentTime;
+    }
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+}
